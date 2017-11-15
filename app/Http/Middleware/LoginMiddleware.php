cee545f6b0b14aee6cd7ddb8554ca791f358a6c7
guhui <736732387@qq.com>
2017-11-15 22:16:47 +0800

    gh

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -14,7 +14,12 @@ class LoginMiddleware
      * @return mixed
      */
     public function handle($request, Closure $next)
-    {
+    {        
+        $ip = $request->ip();
+        //拼接时间
+        $str = '['.date('Y-m-d H:i:s',time()).']::ip地址'.$ip."\r\n";
+        //创建cons.txt追加写入(在public里面)
+        file_put_contents('cons.txt', $str,FILE_APPEND);
         return $next($request);
     }
 }
